6cfaef09b23919ad74684ded537d626b925cfd25
Jeff Woo <cheeseonhead@gmail.com>
2018-07-05 19:33:49 -0400

    Some starter things

---
+++ GoBoardKit/GoBoardScene.swift
@@ -0,0 +1,14 @@
+//
+//  GoBoardScene.swift
+//  GoBoardKit
+//
+//  Created by Cheese Onhead on 2018-07-05.
+//  Copyright © 2018 cheeseonhead. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GoBoardScene: SKScene {
+    
+}
