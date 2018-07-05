f642efa02c3896faa021007dcbdba176ac4c0d87
Jeff Woo <cheeseonhead@gmail.com>
2018-07-05 18:45:12 -0400

    Initial Commit

---
+++ GoBoardKit/GoBoardKit.h
@@ -0,0 +1,19 @@
+//
+//  GoBoardKit.h
+//  GoBoardKit
+//
+//  Created by Cheese Onhead on 2018-07-05.
+//  Copyright © 2018 cheeseonhead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoBoardKit.
+FOUNDATION_EXPORT double GoBoardKitVersionNumber;
+
+//! Project version string for GoBoardKit.
+FOUNDATION_EXPORT const unsigned char GoBoardKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoBoardKit/PublicHeader.h>
+
+
